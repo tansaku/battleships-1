3d2610646128863b419233c80b70adbcea0220bf
Jamie Patel <jamiepatel@Jamies-MacBook-Pro.local>
2014-06-03 19:57:40 +0100

    updated coordinates

--- spec/coordinate_spec.rb
+++ spec/coordinate_spec.rb
@@ -11,4 +11,15 @@ describe Coordinate do
 		expect(coordinate.row).to eq 1
 		expect(coordinate.column).to eq 1
 	end
+
+	it "B1 has a row value of 2 and column 1" do
+		coordinate = Coordinate.new("B", "1")
+		expect(coordinate.row).to eq 2
+		expect(coordinate.column).to eq 1
+	end
+
+	it "knows when it's been targeted" do 
+		player = double :player, target_coordinate: coordinate.targeted = true
+		expect {player.target_coordinate}.to change{coordinate.targeted?}.from(false).to(true)
+	end
 end
